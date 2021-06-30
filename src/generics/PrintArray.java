a7c5b5e8c54a42e57602d46fa89bb18542d74a47
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-30 08:09:11 -0700

    Added:To find maximum number in  three integers

--- src/generics/PrintArray.java
+++ src/generics/PrintArray.java
@@ -4,6 +4,12 @@ public class PrintArray
 {
 	public static void main(String[] args)
 	{
-	  System.out.println("Welcome to generics method program");  
+		int a = 20 , b= 30 , c= 25;
+		if ( a>b && a>c )
+		System.out.println(a+ " is the largest number");
+		else if ( b>a && b>c)
+		System.out.println(b+  " is the largest number");
+		else
+		System.out.println(c+  " is the largest number"); 
 	}
 }
